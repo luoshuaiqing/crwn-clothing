36d5d31f36f3a8864e746982dc681a240a4aebc4
Shuaiqing Luo <shuaiqin@usc.edu>
2021-03-15 23:25:02 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 const App = () => (
   <div>
     <Switch>
       <Route exact path='/' component={HomePage}></Route>
-      <Route path='/hats' component={HatsPage}></Route>
+      <Route path='/shop' component={ShopPage}></Route>
     </Switch>
-  </div> 
+  </div>
 )
 
 export default App;
