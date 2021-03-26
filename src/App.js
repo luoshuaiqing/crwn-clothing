ff15dc0fc627e24b170b203713c92ebda0d575d3
Shuaiqing Luo <shuaiqin@usc.edu>
2021-03-26 11:27:32 -0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import AuthenticatePage from './pages/authenticate/authenticate.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -22,9 +22,8 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
     });
   }
 
