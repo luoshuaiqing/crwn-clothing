f1a7474877891a1e9732cdfc2e6546e98d02b90d
Shuaiqing Luo <shuaiqin@usc.edu>
2021-04-28 21:27:13 -0700

    update

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = () => (
+    <div className="category">
+        <h2>Category Page</h2>
+    </div>
+)
+
+export default CollectionPage;
